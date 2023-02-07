8b3a3ab57ebc4748ae81f8608fdde0321a900074
Andrey <65368541+GorizontX@users.noreply.github.com>
2023-02-07 17:43:11 +0100

    1. Настроил форму светофора 2. Сделал настройку экрана 3. Настроил кнопку, чтобы загорался красный свет

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redImageView: UIImageView!
+    @IBOutlet var yellowImageView: UIImageView!
+    @IBOutlet var greenImageView: UIImageView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redImageView.layer.cornerRadius = 75
+        yellowImageView.layer.cornerRadius = 75
+        greenImageView.layer.cornerRadius = 75
+        startButton.layer.cornerRadius = 10
+        
+        redImageView.alpha = 0.3
+        yellowImageView.alpha = 0.3
+        greenImageView.alpha = 0.3
+        
     }
-
-
+    
+    @IBAction func trafficLightStarted() {
+        redImageView.alpha = 1
+        
+    }
+    
 }
 
+ 
